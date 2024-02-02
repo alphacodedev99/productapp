02d6f1f42e13adabe274d9be40c2785b77bc6902
alphacodedev99 <kiza.milenkovic@gmail.com>
2024-02-02 20:16:57 +0100

    Navbar&Product page...

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import {
+	RouterProvider,
+	createBrowserRouter,
+} from 'react-router-dom';
+
+// store
+import store from './store/store.js';
+
+// pages
+import HomePage from './pages/HomePage.jsx';
+import PostPage from './pages/PostPage.jsx';
+import PostDetailPage from './pages/PostDetailPage.jsx';
+import { Provider } from 'react-redux';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		children: [
+			{
+				path: '/',
+				element: <HomePage />,
+			},
+			{
+				path: '/posts',
+				element: <PostPage />,
+			},
+			{
+				path: '/posts/:id',
+				element: <PostDetailPage />,
+			},
+		],
+	},
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+	<React.StrictMode>
+		<Provider store={store}>
+			<RouterProvider router={router} />
+		</Provider>
+	</React.StrictMode>
+);
---
+++ src/services/productService.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+
+class ProductService {
+    static getAllProducts = () => axios.get('https://dummyjson.com/products');
+}
+
+export default ProductService;
